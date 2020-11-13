df74386a6922557794fb406ec8b2ec19131e1462
Yongkang Zhao <cookieyong@gmail.com>
2020-11-13 13:49:07 -0800

    learning some differences between jsx and html, also some react community conventions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hello World!</div>;
-
+    return (
+        <div>
+            <label class="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor:'blue', color:'white'}}>Submit</button>
+        </div>
+    );
 };
 
 // Take the react component and show it on the screen
