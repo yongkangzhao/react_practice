cbaa532c309fb83ddd2f4136846f52a4d3b791d6
Yongkang Zhao <cookieyong@gmail.com>
2020-11-14 12:41:59 -0800

    improved the button by passing the text value instead of hardcoding the code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+  return 'click on me!';  
+};
+
 // Create a react component
 const App = () => {
-    const buttonText = 'Click Me!';
     return (
         <div>
             <label className="label" for="name">
@@ -12,7 +15,7 @@ const App = () => {
             </label>
             <input id="name" type="text" />
             <button style={{backgroundColor:'blue', color:'white'}}>
-                {buttonText}
+                {getButtonText()}
             </button>
         </div>
     );
